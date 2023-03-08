a243f9f50652b16373bfe4ff7ace0ed0775b3d40
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-08 11:16:41 +0800

    提交LeetCode203题代码

---
+++ src/com/zhuang/datastructure/linkedlist/E02Leetcode203.java
@@ -0,0 +1,83 @@
+package com.zhuang.datastructure.linkedlist;
+
+/**
+ * 根据值删除节点
+ */
+public class E02Leetcode203 {
+
+    /**
+     * https://leetcode.cn/problems/remove-linked-list-elements/
+     *
+     * @param head 链表头
+     * @param val  目标值
+     * @return 删除后的链表头
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        // 哨兵节点
+        ListNode sential = new ListNode(-1, head);
+        ListNode p1 = sential;
+        ListNode p2 = sential.next;
+        while (p2 != null) {
+            // 比较值
+            if (p2.val == val) {
+                // 删除 p2向后移
+                p1.next = p2.next;
+                p2 = p2.next;
+            } else {
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+        }
+        return sential.next;
+    }
+
+    /**
+     * 方法1
+     *
+     * @param head 链表头
+     * @param val  目标值
+     * @return 删除后的链表头
+     */
+    public ListNode removeElements1(ListNode head, int val) {
+        ListNode s = new ListNode(-1, head);
+        ListNode p1 = s;
+        ListNode p2;
+        while ((p2 = p1.next) != null) {
+            if (p2.val == val) {
+                // 删除, p2 向后平移
+                p1.next = p2.next;
+            } else {
+                // p1 p2 向后平移
+                p1 = p1.next;
+            }
+        }
+        return s.next;
+    }
+
+    /**
+     * 方法2
+     *
+     * @param p   链表头
+     * @param val 目标值
+     * @return 删除后的链表头
+     */
+    public ListNode removeElements2(ListNode p, int val) {
+        if (p == null) {
+            return null;
+        }
+        if (p.val == val) {
+            return removeElements2(p.next, val);
+        } else {
+            p.next = removeElements2(p.next, val);
+            return p;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = ListNode.of(1, 2, 6, 3, 6);
+//        ListNode head = ListNode.of(7, 7, 7, 7);
+        System.out.println(head);
+        System.out.println(new E02Leetcode203()
+                .removeElements(head, 6));
+    }
+}
