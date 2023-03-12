574fec1f2360277a0e4b8a51c46bb7d8d2bdd58e
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-12 20:53:30 +0800

    更新代码

--- src/com/zhuang/datastructure/linkedlist/ListNode.java
+++ src/com/zhuang/datastructure/linkedlist/ListNode.java
@@ -8,20 +8,19 @@ package com.zhuang.datastructure.linkedlist;
  */
 public class ListNode {
 
-    int val;
+    public int val;
 
-    ListNode next;
+    public ListNode next;
 
-
-    ListNode() {
+    public ListNode() {
     }
 
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
